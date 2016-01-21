ef381f8d729b2b94ca855799499ce2e2362fa479
twilczek <twilczek@thebrain.pro>
2016-01-21 13:16:09 +0100

    Added Home.jsx with simple Hello World

---
+++ both/components/Home.jsx
@@ -0,0 +1,7 @@
+Home = React.createClass({
+    render() {
+        return <div>
+            <h1>Hello, World</h1>
+        </div>
+    }
+});
---
+++ both/router.jsx
@@ -0,0 +1,17 @@
+Routes = [
+    {
+        url: '/',
+        title: 'TheBrain',
+        name: 'home',
+        action() {
+            ReactLayout.render(Home);
+        }
+    }
+];
+
+_.each(Routes, (route) => {
+    FlowRouter.route(route.url, {
+        name: route.name,
+        action: route.action
+    });
+});
