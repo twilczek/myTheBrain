4c44a4c0fa5d36f372865c0fa6182faf7771bca1
twilczek <twilczek@thebrain.pro>
2016-01-21 14:07:35 +0100

    added stylesheets from thebrain.pro

--- both/components/Home.jsx
+++ both/components/Home.jsx
@@ -1,6 +1,7 @@
 Home = React.createClass({
     render() {
         return <div>
+            <Navbar/>
             <h1>Hello, World</h1>
         </div>
     }
---
+++ both/components/Navbar.jsx
@@ -0,0 +1,15 @@
+Navbar = React.createClass({
+    render() {
+        return (
+        <div>
+            <nav className="navbar navbar-inverse">
+                <ul>
+                    <li>Navbar</li>
+                    <li>Navbar</li>
+                    <li>Navbar</li>
+                    <li>Navbar</li>
+                </ul>
+            </nav>
+        </div>
+        )}
+});
