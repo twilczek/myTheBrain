581d48d14e952694266dd777a609fc7139a3eb32
twilczek <twilczek@thebrain.pro>
2016-01-22 13:46:43 +0100

    Added Footer

---
+++ both/components/Footer.jsx
@@ -0,0 +1,19 @@
+Footer = React.createClass({
+   render(){
+       return(
+           <div id="footer">
+               <p class="hidden-xs">
+                   2013 TheBrain by TheBrain Software House <span class="font-grey-4">|</span>
+                   Contact us at lgandecki AT TheBrain.Pro <span class="font-grey-4">|</span> <a
+                   href="#">Policy</a>
+                   <span class="font-grey-4">|</span> <a href="#" target="_blank">TheBrain.Pro
+                   Blog</a>
+               </p>
+               <p class="visible-xs">
+                   TheBrain by <a href="#">TheBrain Software House</a>
+               </p>
+               <a href="#" class="gototop"><i class="fa fa-arrow-up"></i></a>
+           </div>
+       )
+   }
+});
