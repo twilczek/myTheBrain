8829d32b5dffb853a4e91bb06d28410bfafca0ee
twilczek <twilczek@thebrain.pro>
2016-02-02 11:48:51 +0100

    Navigation content finished + added accounts-password

--- both/components/Navbar.jsx
+++ both/components/Navbar.jsx
@@ -2,18 +2,47 @@ NavBar = React.createClass({
     render() {
         return (
             <div>
-                <nav className="navbar navbar-inverse">
-                    <div className="container-fluid">
-                        <div className="navbar-default">
-                            <ul className="nav navbar-nav navbar-left">
-                                <li><a href="#">Sign up!</a></li>
-                                <li><a href="#">Flashcards</a></li>
-                                <li><a href="#">Courses</a></li>
-                                <li><a href="#">KhanAcademy Playlists</a></li>
-                            </ul>
+                <nav id="navigation" className="navbar navbar-fixed-top">
+                        <div className="container-fluid">
+                            <a href="/" id="myBrand" alt="TheBrain">&nbsp;</a>
+                            <div className="navbar-default">
+                                <ul className="main-nav nav navbar-nav navbar-left">
+                                    <li className="active">
+                                        <a href="#"><b>Sign up!</b>
+                                        </a>
+                                    </li>
+                                    <li className="dropdown">
+                                        <a className="dropdown-toggle" data-toggle="dropdown" href="#">Flashcards
+                                            <span className="caret"></span>
+                                        </a>
+                                        <ul className="dropdown-menu dropdown-content">
+                                            <li>
+                                                <a href="#">Look for Flashcards</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li className="dropdown">
+                                        <a className="dropdown-toggle" data-toggle="dropdown"  href="#">Courses
+                                            <span className="caret"></span>
+                                        </a>
+                                        <ul className="dropdown-menu dropdown-content">
+                                            <li>
+                                                <a href="#">Find available courses</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="#">KhanAcademy Playlists</a>
+                                    </li>
+                                </ul>
+                                <div className="login-button">
+                                    <button type="button" id="login-button" className="pull-right btn btn-primary btn-header btn-logInModal">
+                                        <b>Log In!</b>
+                                    </button>
+                                </div>
+                            </div>
                         </div>
-                    </div>
                 </nav>
             </div>
-        )}
-});
+                )}
+        });
