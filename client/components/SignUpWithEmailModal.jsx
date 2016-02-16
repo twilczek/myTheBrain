1e413571feb9a214180c5ebcf5ffec7970efeac2
twilczek <twilczek@thebrain.pro>
2016-02-16 13:18:06 +0100

    Added checking whether passwords are equal

--- client/components/SignUpWithEmailModal.jsx
+++ client/components/SignUpWithEmailModal.jsx
@@ -3,7 +3,8 @@ SignUpWithEmailModal = React.createClass({
     getInitialState: function() {
         return {
             email: "",
-            password: ""
+            password: "",
+            confirmationPassword: ""
         }
     },
     handleChangeEmail: function(event) {
@@ -14,9 +15,19 @@ SignUpWithEmailModal = React.createClass({
         console.log('handleChangePassword');
         this.setState({ password: event.target.value });
     },
+    handleChangePasswordConfirmation: function(event) {
+        console.log('confirmationPassword');
+        this.setState({ confirmationPassword: event.target.value });
+    },
     resetState: function(){
         console.log('onClickClose');
         this.setState({  email: "", password: ""})
+    },
+    checkIfPasswordsMatch: function(password=this.state.password,confirmationPassword=this.state.confirmationPassword){
+        return password === confirmationPassword ? true : false;
+    },
+    submitRegistrationData: function(event){
+
     },
 
     render() {
@@ -39,21 +50,29 @@ SignUpWithEmailModal = React.createClass({
                                     <h4 className="modal-title">Sign up!</h4>
                                 </div>
                                 <div className="modal-body">
-                                    <form id="login-form" action="action">
+                                    <form id="login-form" action="action" onSubmit={this.submitRegistrationData}>
                                         <div className="form-group">
                                             <label htmlFor="recipient-name" className="control-label">Email Address</label>
                                             <input type="text" className="form-control" id="user-id" onChange={this.handleChangeEmail} value={this.state.email} />
                                         </div>
                                         <div className="form-group">
                                             <label htmlFor="recipient-name" className="control-label"> Password</label>
-                                            <input type="text" className="form-control" id="user-id" onChange={this.handleChangePassword} value={this.state.password}/>
+                                            <input type="password" className="form-control" id="user-id" onChange={this.handleChangePassword} value={this.state.password}/>
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="recipient-name" className="control-label"> Re-type Password</label>
+                                            <input type="password" className="form-control" id="user-id" onChange={this.handleChangePasswordConfirmation} value={this.state.confirmationPassword}/>
+                                            {(!this.checkIfPasswordsMatch())? <span><small>Passwords do not match!</small></span> : null}
+                                        </div>
+                                        <div className="form-group">
+                                            <div className="modal-footer">
+                                                <button type="button" className="btn btn-default" data-dismiss="modal" onClick={this.resetState}>Close</button>
+                                                <button type="submit" className="login-button btn btn-primary" disabled={this.state.email.length === 0 || this.state.password.length === 0 || this.state.confirmationPassword.length === 0 }>Sign up!</button>
+                                            </div>
                                         </div>
                                     </form>
                                 </div>
-                                <div className="modal-footer">
-                                    <button type="button" className="btn btn-default" data-dismiss="modal" onClick={this.resetState}>Close</button>
-                                    <button type="submit" className="login-button btn btn-primary" disabled={this.state.email.length === 0 || this.state.password.length === 0 }>Sign up!</button>
-                                </div>
+
                             </div>
                     </div>
                 </div>
