5eec085a311bb37438f0b6282939bf81f1dc21b4
twilczek <twilczek@thebrain.pro>
2016-02-22 12:43:34 +0100

    Fully functional registration

--- client/components/SignUpWithEmailModal.jsx
+++ client/components/SignUpWithEmailModal.jsx
@@ -19,33 +19,53 @@ SignUpWithEmailModal = React.createClass({
     handleCredentialsChange: function(input, event){
         this.setState({[input]: event.target.value})
     },
-    checkPasswordLength: function(){
+    renderPasswordLengthWarning: function(){
         if(this.state.password.length < 6){
             return (<span className="warning"><small>The password
                 is too short</small></span>);
         }
-        else return null;
+        else return false;
     },
-    checkIfPasswordsMatch: function(password=this.state.password,confirmationPassword=this.state.confirmationPassword){
-        if (password !== confirmationPassword){
+    renderPasswordMismatchWarning: function(){
+        if (this.state.password !== this.state.confirmationPassword){
             return (<span className="warning"><small>The passwords
                 do not match </small></span>);
         }
         else {
-            return null;
+            return false;
         }
     },
-    checkEmailAndSubmit: function(event){
-        event.preventDefault()
-        var userName = this.state.email;
-        var atPosition = userName.indexOf('@');
+    prepareUserName: function(email){
+        var userName;
+        var atPosition = email.indexOf('@');
         if (atPosition > -1) {
-            userName = userName.slice(0, atPosition);
-            this.submitRegistrationData(userName);
+            userName = email.slice(0, atPosition);
+            return userName;
+        } else return false;
+    },
+    submitRegistrationData: function(event){
+        event.preventDefault();
+        var userName= this.prepareUserName(this.state.email);
+        if (userName) {
+            Accounts.createUser({
+                    email: this.state.email,
+                    password: this.state.password,
+                    username: userName,
+                    profile: {
+                        name: userName
+                    }
+                },
+                function (error, result) {
+                    if (error) {
+                        console.log("error", error);
+                        console.log("result", result);
+                    }
+                }
+            );
             $('#registerModal').modal('hide');
         }
         else {
-            alert("Incorrect email address");
+            alert("Check whether email address is correct");
         }
     },
     changePasswordFocus: function(value){
@@ -54,23 +74,13 @@ SignUpWithEmailModal = React.createClass({
     changeConfirmationFocus: function(value){
         this.setState({confirmationHasFocus: value})
     },
-    submitRegistrationData: function (userName) {
-        Accounts.createUser({
-                email: this.state.email,
-                password: this.state.password,
-                username: userName,
-                profile: {
-                    name: userName
-                }
-            },
-            function (error, result) {
-                if (error) {
-                    console.log("error", error);
-                    console.log("result", result);
-                }
-            }
-        )
+    shouldButtonBeDisabled: function() {
+        return !this.renderPasswordLengthWarning() && /*!!this.prepareUserName(this.state.email) &&*/ !this.renderPasswordMismatchWarning();
     },
+    //  NIE DZIALA ON CHANGE W FORMIE
+    // seeOnChange: function(event){
+    //    console.log("w onChange", event);
+    //},
 
     render() {
         return (
@@ -92,7 +102,7 @@ SignUpWithEmailModal = React.createClass({
                                     <h4 className="modal-title">Sign up!</h4>
                                 </div>
                                 <div className="modal-body">
-                                    <form id="login-form" action="action" onSubmit={this.checkEmailAndSubmit}>
+                                    <form id="login-form" action="action" onSubmit={this.submitRegistrationData}>
                                         <div className="form-group">
                                             <label htmlFor="recipient-name" className="control-label">Email Address</label>
                                             <input type="text" className="form-control" id="user-id" onChange={this.handleCredentialsChange.bind(this, 'email')} value={this.state.email} />
@@ -100,17 +110,17 @@ SignUpWithEmailModal = React.createClass({
                                         <div className="form-group">
                                             <label htmlFor="recipient-name" className="control-label"> Password</label>
                                             <input type="password" className="form-control" id="user-id" onChange={this.handleCredentialsChange.bind(this, 'password')} value={this.state.password} onFocus={this.changePasswordFocus.bind(this,true)} onBlur={this.changePasswordFocus.bind(this,false)}/>
-                                            {this.state.passwordHasFocus ? this.checkPasswordLength() : null}
+                                            {this.state.passwordHasFocus ? this.renderPasswordLengthWarning() : null}
                                         </div>
                                         <div className="form-group">
                                             <label htmlFor="recipient-name" className="control-label"> Re-type Password</label>
                                             <input type="password" className="form-control" id="user-id" onChange={this.handleCredentialsChange.bind(this, 'confirmationPassword')} value={this.state.confirmationPassword} onFocus={this.changeConfirmationFocus.bind(this,true)} onBlur={this.changeConfirmationFocus.bind(this,false)}/>
-                                            {this.state.confirmationHasFocus ? this.checkIfPasswordsMatch() : null}
+                                            {this.state.confirmationHasFocus ? this.renderPasswordMismatchWarning() : null}
                                         </div>
                                         <div className="form-group">
                                             <div className="modal-footer">
                                                 <button type="button" className="btn btn-default" data-dismiss="modal" onClick={this.resetState}>Close</button>
-                                                <button type="submit" className="login-button btn btn-primary" disabled={this.state.email.length < 6 || !this.checkIfPasswordsMatch() || !this.checkPasswordLength()}>Sign up!</button>
+                                                <button type="submit" className="login-button btn btn-primary" disabled={!this.shouldButtonBeDisabled()}>Sign up!</button>
                                             </div>
                                         </div>
                                     </form>
